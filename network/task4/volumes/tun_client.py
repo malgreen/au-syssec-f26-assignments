5b37516e2335e8eccee8f3a735decaeb9cabf2cc
Martin Algreen <git@mal.green>
2026-03-20 09:26:48 +0100

    task4: use functions

--- network/task4/volumes/tun_client.py
+++ network/task4/volumes/tun_client.py
@@ -8,57 +8,63 @@ import time
 
 from scapy.all import *
 
-# ==> SOCKET <==
 SERVER_IP = "10.9.0.11"
 SERVER_PORT = 9090
 
-tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ssl_ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-ssl_ctx.load_verify_locations("./cert.pem")
-ssl_sock = ssl_ctx.wrap_socket(tcp_sock, server_hostname=SERVER_IP)
-
-while True:
-    try:
-        print("Trying to connect...")
-        ssl_sock.connect((SERVER_IP, SERVER_PORT))
-        print("SSL connected")
-        break  # just exit the loop on successful connection
-    except KeyboardInterrupt:
-        exit(0)
-    except:
-        time.sleep(1)
-
-
-# ==> TUN <==
 TUNSETIFF = 0x400454CA
 IFF_TUN = 0x0001
 IFF_TAP = 0x0002
 IFF_NO_PI = 0x1000
 
-# ==> Interface
-tun = os.open("/dev/net/tun", os.O_RDWR)
-ifr = struct.pack("16sH", b"tun%d", IFF_TUN | IFF_NO_PI)
-ifname_bytes = fcntl.ioctl(tun, TUNSETIFF, ifr)
-ifname = ifname_bytes.decode("UTF-8")[:16].strip("\x00")
-print("Interface Name: {}".format(ifname))
 
-# ==> Routing
-os.system(f"ip link set dev {ifname} up")
-os.system(f"ip addr add 192.168.53.99/24 dev {ifname}")
-os.system(f"ip route add 192.168.60.0/24 dev {ifname} onlink via {SERVER_IP}")
+def main():
+    tun_dev = setup_tun()
+    ssl_sock = setup_ssl()
+    while True:
+        ready, _, _ = select.select([ssl_sock, tun_dev], [], [])
+        for fd in ready:
+            if fd is ssl_sock:  # === INBOUND ===
+                data = ssl_sock.recv(2048)
+                pkt = IP(data)
+                print(f"[INBOUND] <==: {pkt.src} --> {pkt.dst}")
+                os.write(tun_dev, bytes(pkt))
+
+            if fd is tun_dev:  # === OUTBOUND ===
+                packet = os.read(tun_dev, 2048)
+                pkt = IP(packet)
+                print(f"[OUTBOUND] ==>: {pkt.src} --> {pkt.dst}")
+                ssl_sock.send(bytes(pkt))
 
 
-while True:
-    ready, _, _ = select.select([ssl_sock, tun], [], [])
-    for fd in ready:
-        if fd is ssl_sock:  # === INBOUND ===
-            data = ssl_sock.recv(2048)
-            pkt = IP(data)
-            print(f"[INBOUND] <==: {pkt.src} --> {pkt.dst}")
-            os.write(tun, bytes(pkt))
+def setup_tun():
+    tun = os.open("/dev/net/tun", os.O_RDWR)
+    ifr = struct.pack("16sH", b"tun%d", IFF_TUN | IFF_NO_PI)
+    ifname_bytes = fcntl.ioctl(tun, TUNSETIFF, ifr)
+    ifname = ifname_bytes.decode("UTF-8")[:16].strip("\x00")
+    print("Interface Name: {}".format(ifname))
+    os.system(f"ip link set dev {ifname} up")
+    os.system(f"ip addr add 192.168.53.99/24 dev {ifname}")
+    os.system(f"ip route add 192.168.60.0/24 dev {ifname} onlink via {SERVER_IP}")
+    return tun
 
-        if fd is tun:  # === OUTBOUND ===
-            packet = os.read(tun, 2048)
-            pkt = IP(packet)
-            print(f"[OUTBOUND] ==>: {pkt.src} --> {pkt.dst}")
-            ssl_sock.send(bytes(pkt))
+
+def setup_ssl() -> ssl.SSLSocket:
+    tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ssl_ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_ctx.load_verify_locations("./cert.pem")
+    ssl_sock = ssl_ctx.wrap_socket(tcp_sock, server_hostname=SERVER_IP)
+    while True:
+        try:
+            print("Trying to connect...")
+            ssl_sock.connect((SERVER_IP, SERVER_PORT))
+            print("SSL connected")
+            break  # just exit the loop on successful connection
+        except KeyboardInterrupt:
+            exit(0)
+        except:
+            time.sleep(1)
+    return ssl_sock
+
+
+if __name__ == "__main__":
+    main()
--- network/task4/volumes/tun_server.py
+++ network/task4/volumes/tun_server.py
@@ -1,57 +1,67 @@
 #!/usr/bin/env python3
 import fcntl
+from re import sub
 import ssl
-
+import os
+import subprocess
 from scapy.all import *
 
-
-# ==> SOCKET <==
 PORT = 9090
-
-RX_IP = "0.0.0.0"
-tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_sock.bind((RX_IP, PORT))
-tcp_sock.listen()
-print("Waiting for TCP connection...")
-tcp_conn, _ = tcp_sock.accept()
-print("TCP client connected")
-ssl_ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_ctx.load_cert_chain("./cert.pem", "./key.pem")
-ssl_ctx.verify_mode = ssl.CERT_NONE
-ssl_sock = ssl_ctx.wrap_socket(tcp_conn, server_side=True)
-
-# ==> TUN <==
+CLIENT_IP_EXTERNAL = "10.9.0.5"
+CLIENT_IP_INTERNAL = "192.168.53.99"
+LISTEN_IP = "0.0.0.0"
 TUNSETIFF = 0x400454CA
 IFF_TUN = 0x0001
 IFF_TAP = 0x0002
 IFF_NO_PI = 0x1000
 
-# ==> Interface
-tun = os.open("/dev/net/tun", os.O_RDWR)
-ifr = struct.pack("16sH", b"tun%d", IFF_TUN | IFF_NO_PI)
-ifname_bytes = fcntl.ioctl(tun, TUNSETIFF, ifr)
-ifname = ifname_bytes.decode("UTF-8")[:16].strip("\x00")
-print("Interface Name: {}".format(ifname))
 
-# ==> Routing
-os.system(f"ip link set dev {ifname} up")
-os.system(f"ip addr add 192.168.60.11/24 dev {ifname}")
+def main():
+    tun_dev = setup_tun()
+    ssl_sock = setup_ssl()
+    while True:
+        ready, _, _ = select.select([ssl_sock, tun_dev], [], [])
+        for fd in ready:
+            if fd is ssl_sock:  # === INBOUND ===
+                data = ssl_sock.recv(2048)
+                pkt = IP(data)
+                print(f"[INBOUND] <==: {pkt.src} --> {pkt.dst}")  # pyright: ignore[reportUnknownMemberType]
+                os.write(tun_dev, bytes(pkt))
 
-CLIENT_IP = "10.9.0.5"
-os.system(f"ip route add 192.168.53.99 dev {ifname} onlink via {CLIENT_IP}")
+            if fd is tun_dev:  # === OUTBOUND ===
+                packet = os.read(tun_dev, 2048)
+                pkt = IP(packet)
+                print(f"[OUTBOUND] ==>: {pkt.src} --> {pkt.dst}")
+                ssl_sock.send(bytes(pkt))
 
 
-while True:
-    ready, _, _ = select.select([ssl_sock, tun], [], [])
-    for fd in ready:
-        if fd is ssl_sock:  # === INBOUND ===
-            data = ssl_sock.recv(2048)
-            pkt = IP(data)
-            print(f"[INBOUND] <==: {pkt.src} --> {pkt.dst}")  # pyright: ignore[reportUnknownMemberType]
-            os.write(tun, bytes(pkt))
+def setup_tun():
+    tun = os.open("/dev/net/tun", os.O_RDWR)
+    ifr = struct.pack("16sH", b"tun%d", IFF_TUN | IFF_NO_PI)
+    ifname_bytes = fcntl.ioctl(tun, TUNSETIFF, ifr)
+    ifname = ifname_bytes.decode("UTF-8")[:16].strip("\x00")
+    print("Interface Name: {}".format(ifname))
+    os.system(f"ip link set dev {ifname} up")
+    os.system(f"ip addr add 192.168.60.11/24 dev {ifname}")
+    os.system(
+        f"ip route add {CLIENT_IP_INTERNAL} dev {ifname} onlink via {CLIENT_IP_EXTERNAL}"
+    )
+    return tun
 
-        if fd is tun:  # === OUTBOUND ===
-            packet = os.read(tun, 2048)
-            pkt = IP(packet)
-            print(f"[OUTBOUND] ==>: {pkt.src} --> {pkt.dst}")
-            ssl_sock.send(bytes(pkt))
+
+def setup_ssl() -> ssl.SSLSocket:
+    tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_sock.bind((LISTEN_IP, PORT))
+    tcp_sock.listen()
+    print("Waiting for TCP connection...")
+    tcp_conn, _ = tcp_sock.accept()
+    print("TCP client connected")
+    ssl_ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_ctx.load_cert_chain("./cert.pem", "./key.pem")
+    ssl_ctx.verify_mode = ssl.CERT_NONE
+    ssl_sock = ssl_ctx.wrap_socket(tcp_conn, server_side=True)
+    return ssl_sock
+
+
+if __name__ == "__main__":
+    main()
